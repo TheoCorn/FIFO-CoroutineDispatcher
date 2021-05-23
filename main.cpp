c49df9c08e8f81b2317cfe64315f449a2279d372
TCorn <theo@capek.aero>
2021-05-23 12:14:51 +0200

    init commit

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #include <string>
 
 void print(void* arg){
-//    std::cout<< *reinterpret_cast<std::string*>(arg) <<std::endl;
-//    delete reinterpret_cast<std::string*>(arg);
+    std::cout<< *reinterpret_cast<std::string*>(arg) <<std::endl;
+    delete reinterpret_cast<std::string*>(arg);
 
     std::cout<<"runnable" <<std::endl;
 }
@@ -17,19 +17,17 @@ int main() {
     mtl::Looper looper;
     std::function<void(void *)> fun = print;
 
-    auto runnable = new mtl::Runnable(fun, nullptr, true);
-    looper.add(runnable);
 
-
-//    for (int i = 0; i < 100; ++i) {
-//        mtl::Runnable runnable(fun, nullptr, true);
-////        std::string str = std::to_string(i);
-//        looper.add(&runnable);
-//    }
+    for (int i = 0; i < 100; ++i) {
+        auto str = new std::string;
+        *str = std::to_string(i);
+        auto runnable = new mtl::Runnable(fun, str, true);
+        looper.add(runnable);
+    }
 
 
     looper.run();
-    std::this_thread::sleep_for(std::chrono::seconds(60));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     looper.stopAndJoin();
 
 }
