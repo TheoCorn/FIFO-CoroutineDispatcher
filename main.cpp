a5d99aeafac1fac36a2feb45a4aaa280987057e2
TCorn <theo@capek.aero>
2021-05-23 12:11:04 +0200

    init commit

--- main.cpp
+++ main.cpp
@@ -17,16 +17,19 @@ int main() {
     mtl::Looper looper;
     std::function<void(void *)> fun = print;
 
+    auto runnable = new mtl::Runnable(fun, nullptr, true);
+    looper.add(runnable);
 
-    for (int i = 0; i < 100; ++i) {
-        std::string str = std::to_string(i);
-        mtl::Runnable runnable(fun, nullptr, true);
-        looper.add(&runnable);
-    }
+
+//    for (int i = 0; i < 100; ++i) {
+//        mtl::Runnable runnable(fun, nullptr, true);
+////        std::string str = std::to_string(i);
+//        looper.add(&runnable);
+//    }
 
 
     looper.run();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(60));
     looper.stopAndJoin();
 
 }
